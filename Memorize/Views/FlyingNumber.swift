bb486cd4fa7905eb3270b5b6afe880bf4f9fd048
nnnniicckkkk <nickhurstdev28@gmail.com>
2024-05-03 13:57:16 -0700

    added flip animation with explicit animations to ensure card flip is smooth and view does not ease in on its own
    started adding a func to show score and have it slide up when the score changes from the card

---
+++ Memorize/Views/FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  Memorize
+//
+//  Created by Nicholas Hurst on 5/3/24.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
